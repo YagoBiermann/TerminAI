76394e301eb6a4a9c3871106c93adc6c0e6f11ca
YagoBiermann <bsfyagobsf@gmail.com>
2025-02-27 00:56:44 -0300

    refactor: move spinner class into its own file

--- main.py
+++ main.py
@@ -20,35 +20,6 @@ EXIT_WORDS = [
 ]
 exit_pattern = rf"\b({'|'.join(re.escape(word) for word in EXIT_WORDS)})(?:[\s,!.?:;]+{re.escape(AI_NAME)})?[\s!?.:;]*\b"
 
-class Spinner:
-  spinner_chars = ['-', '\\', '|', '/']
-
-  def __init__(self, delay: float = 0.1):
-    self.delay = delay
-    self.stop_event = threading.Event()
-    self.thread = threading.Thread(target=self._spin)
-  
-  def _spin(self):
-    while not self.stop_event.is_set():
-      for symbol in self.spinner_chars:
-        if self.stop_event.is_set():
-          break
-        sys.stdout.write(f"\r {symbol} ")
-        sys.stdout.flush()
-        time.sleep(self.delay)
-    sys.stdout.write("\r" + " " * 20 + "\r")
-    sys.stdout.flush()
-
-  def __enter__(self):
-    self.thread.start()
-    return self
-
-  def __exit__(self, exc_type, exc_value, traceback):
-    self.stop_event.set()
-    self.thread.join()
-    sys.stdout.write("\r" + " " * 20 + "\r")
-    sys.stdout.flush()
-
 def handle_user_interaction(chat_history: list, user_message: str):
   with Spinner():
     try:
---
+++ spinner.py
@@ -0,0 +1,33 @@
+import sys
+import threading
+import time
+
+
+class Spinner:
+  spinner_chars = ['-', '\\', '|', '/']
+
+  def __init__(self, delay: float = 0.1):
+    self.delay = delay
+    self.stop_event = threading.Event()
+    self.thread = threading.Thread(target=self._spin)
+  
+  def _spin(self):
+    while not self.stop_event.is_set():
+      for symbol in self.spinner_chars:
+        if self.stop_event.is_set():
+          break
+        sys.stdout.write(f"\r {symbol} ")
+        sys.stdout.flush()
+        time.sleep(self.delay)
+    sys.stdout.write("\r" + " " * 20 + "\r")
+    sys.stdout.flush()
+
+  def __enter__(self):
+    self.thread.start()
+    return self
+
+  def __exit__(self):
+    self.stop_event.set()
+    self.thread.join()
+    sys.stdout.write("\r" + " " * 20 + "\r")
+    sys.stdout.flush()
