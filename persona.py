013c29fbde460f3f5bbd999b10e4eb0df9eac866
YagoBiermann <bsfyagobsf@gmail.com>
2025-03-02 02:50:53 -0300

    refactor: import rules from a variable instead of json file

--- persona.py
+++ persona.py
@@ -1,6 +1,8 @@
 import json
 import os
 from dotenv import load_dotenv
+from rules import rules
+
 load_dotenv()
 
 def load_file(path):
@@ -14,10 +16,7 @@ def load_file(path):
 
 file_path = os.path.dirname(os.path.abspath(__file__))
 persona_path = os.path.join(file_path, "personas.json")
-rules_path = os.path.join(file_path, "rules.json")
-
 personas = load_file(persona_path)
-rules = load_file(rules_path)
 
 current_persona_key = os.getenv('CURRENT_PERSONA')
 persona_description = ""
@@ -25,14 +24,10 @@ persona_description = ""
 if personas and current_persona_key in personas:
     selected_persona = personas[current_persona_key]
     persona_description = f"""
-    Your name is: {selected_persona['name']}
-    Description: {selected_persona['description']}
+  Your name is: {selected_persona['name']}
+  Description: {selected_persona['description']}
     """
 else:
   print(f"Warning: Persona '{current_persona_key}' not applied. Using default settings")
 
-if rules:
-  persona_description += "\n Response Rules:" + "".join(f"\n- {rules}" for rules in rules['rules'])
-else:
-  print(f"Warning: rules.json not found. Rules not applied.")
-  rules = ""
+persona_description += rules
