24e229c3cf95776eaa526cc3fbe359020e2e2dc8
YagoBiermann <bsfyagobsf@gmail.com>
2025-03-03 03:37:26 -0300

    feat: add function to clear lines above in terminal

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import sys
 
-def ClearLine():
-  sys.stdout.write("\033[F")  # Move cursor up
-  sys.stdout.write("\033[K")  # Clear the line
-  sys.stdout.flush()
+def ClearLinesAbove(n: int = 1):
+    for _ in range(n):
+        sys.stdout.write("\033[F")  # Move cursor up
+        sys.stdout.write("\r\033[K")  # Clear the line
+    sys.stdout.flush()
