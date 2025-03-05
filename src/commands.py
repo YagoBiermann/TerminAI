6a2d4e19a3c3f7ad04630c916db8a0ecfd9766ea
YagoBiermann <bsfyagobsf@gmail.com>
2025-03-04 22:30:00 -0300

    feat: add color to text in confirmation dialog

--- src/commands.py
+++ src/commands.py
@@ -2,20 +2,21 @@ import subprocess
 from src.display_messages import display_ai_response
 from src.spinner import Spinner
 from src.utils import clear_currentLine, clear_lines_above
+from colorama import Fore
 
 EMPTY_OUTPUT_MESSAGE = "No output was returned"
 
 def confirm_command() -> bool:
     while True:
       try:
-        confirmAction = input("Run command? [Y/N]\n").strip().upper()
+        confirmAction = input(f"{Fore.LIGHTBLUE_EX}Run command?{Fore.RESET} [Y/N]\n").strip().upper()
         clear_lines_above(2)
         return True if confirmAction == "Y" else False
       except (KeyboardInterrupt, EOFError):
         break 
 
 def reconfirm_command():
-   display_ai_response("Are you sure?")
+   display_ai_response(f"{Fore.LIGHTRED_EX}Are you sure?{Fore.RESET}")
    return confirm_command()
 
 def run_command(command: str) -> None:
