73cd4d80ccdf8e15118269c28d72b697149bcc4f
YagoBiermann <bsfyagobsf@gmail.com>
2025-03-04 22:16:16 -0300

    refactor: remove useless code

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-import os
-import sys
 from dotenv import load_dotenv
 from src.chat import init_chat
 
 if __name__ == "__main__":
   load_dotenv()
-  sys.path.append(os.path.abspath("src"))
   init_chat()
