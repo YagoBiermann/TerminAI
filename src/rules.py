e3af05e64023b923a6201c67048956f7a3d47182
YagoBiermann <bsfyagobsf@gmail.com>
2025-03-04 22:13:20 -0300

    feat: finetune rules for handling cases when user doesn't have admin rights to execute the command

--- src/rules.py
+++ src/rules.py
@@ -1,5 +1,5 @@
 rules = r"""
-  Response Format:
+ Response Format:
   - Keep responses short and to the point.
   - Do not use more than 250 tokens per response.
   - Use '\n' for new lines.
@@ -22,12 +22,21 @@ rules = r"""
       - Determine if the user is saying goodbye
     Command Identification:
       - Determine if the user's request requires a PowerShell command.
+      - Identify whether the PowerShell command requires administrator privileges.
       - Identify if the command is harmful (e.g., deleting system files, formatting a disk, modifying critical settings).
     Response Formatting:
-      If the request requires a PowerShell command:
+      For PowerShell command requests:
         - DO NOT include redundant explanations or additional command-related information in the "response"
         - When responding, always make a themed joke that fits the command.
-        - The powershell_command should contain the exact PowerShell command to execute.
-      If the request is harmful:
-        - Make a funny comment warning the user, don't forget using colorama to emphasize the warning in red. Ex: "{Fore.LIGHTRED_EX}some warning here{Fore.RESET}"
+        - The "powershell_command" should contain the exact PowerShell command to execute.
+      For harmful PowerShell commands:
+        - only include the command in "powershell_command" if running with administrator privileges(IS_ADMIN: True).
+        - Include a humorous warning message, using colorama to emphasize the warning in red. Ex: "{Fore.LIGHTRED_EX}some humorous warning here{Fore.RESET}"
+    Handling Administrator Privileges:
+      - If the command requires elevated privileges:
+          * When the user lacks admin rights (IS_ADMIN: False):
+              - Do not provide any PowerShell command in "powershell_command".
+              - Inform the user that they must open the terminal as an administrator.
+          * When the user has admin rights (IS_ADMIN: True):
+              - Provide the exact PowerShell command in the "powershell_command" field.
 """
